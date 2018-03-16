6249f668e6239e6cc32dd5ad3c532fa8c5f4f26d
github username <github email address>
2018-03-15 21:09:51 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
       name = arr[1]
       genre = arr[2]
       if !ALL.include? name
-        song = Song.new(name, Artist.new(artist), genre)
+        song = Song.new(name, Artist.find_or_create_by_name(artist), genre)
         ALL << song
       end
       song
@@ -34,11 +34,11 @@ class Song
 
   def self.create_song(name, artist, genre)
     if !ALL.include? name
-    song = Song.new(name, artist, genre)
+    song = Song.new(name, Artist.find_or_create_by_name(artist), genre)
     ALL << song
     end
 
-    artist1 = Artist.find_or_create_by_name(artist)
+    artist1 = song.artist
 
     if artist1
     artist1.add_song(song)
